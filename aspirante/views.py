eb3ccbec911f18ceb79bc910756dd9f190caa10e
jenni-03 <jenniferdayanasr@ufps.edu.co>
2022-11-29 06:55:15 -0500

    modelos y views

--- aspirante/views.py
+++ aspirante/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def  index(request):
     return render(request, 'index.html')
+
+def registrar_aspirante(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        apellido = request.POST['apellido']
+        tipo_documento = request.POST['tipo_documento']
+        numero_documento = request.POST['numero_documento']
+        profesion = request.FILES['profesion']
+        ciudad = request.POST['ciudad']
+        edad = request.POST['edad']
+    return render(request, 'registrar.html')
+
+def listar(request):
+    return render(request, 'listado.html')
